d4b098a5efd42e014c993c315130ed217cde063a
Gabriel Bur <gabriel.bur@outlook.com>
2019-08-21 22:20:42 -0300

    adding operations to calculator

--- AspNetUdemy-01-Calculator/Calculator/Controllers/CalculatorController.cs
+++ AspNetUdemy-01-Calculator/Calculator/Controllers/CalculatorController.cs
@@ -11,8 +11,8 @@ namespace Calculator.Controllers
     [Route("api/Calculator")]
     public class CalculatorController : Controller
     {
-        // Get api/Calculator/5/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        // Get api/Calculator/Sum/5/5
+        [HttpGet("Sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
@@ -22,37 +22,110 @@ namespace Calculator.Controllers
 
                 return Ok(sum.ToString());
             }
+            return BadRequest("Invalid Input");
+        }
 
+
+        // Get api/Calculator/Sub/5/5
+        [HttpGet("Sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) -
+                    ConvertToDecimal(secondNumber);
+
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+        // Get api/Calculator/Multiply/5/5
+        [HttpGet("Multiply/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiply(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multiply = ConvertToDecimal(firstNumber) *
+                    ConvertToDecimal(secondNumber);
+
+                return Ok(multiply.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+        // Get api/Calculator/Div/5/5
+        [HttpGet("Div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var div = ConvertToDecimal(firstNumber) /
+                    ConvertToDecimal(secondNumber);
+
+                return Ok(div.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+        // Get api/Calculator/Mean/5/5
+        [HttpGet("Mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mean = (ConvertToDecimal(firstNumber) +
+                    ConvertToDecimal(secondNumber)) / 2;
+
+                return Ok(mean.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
+        // Get api/Calculator/SquareRoot/5/5
+        [HttpGet("SquareRoot/{firstNumber}")]
+        public IActionResult SquareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                var SquareRoot = ConvertToDecimal(firstNumber);
+
+                return Ok(Math.Sqrt(SquareRoot).ToString());
+            }
             return BadRequest("Invalid Input");
         }
 
 
 
-
-
-        private decimal ConvertToDecimal(string number)
+        #region private
+        private double ConvertToDecimal(string number)
         {
-            decimal result;
+            double result;
 
-            if (decimal.TryParse(number,out result))
+            if (double.TryParse(number, out result))
             {
                 return result;
             }
 
             return 0;
         }
-
         private bool IsNumeric(string number)
         {
             double numberParsed;
 
             bool isNumber = double
-                .TryParse(number, 
+                .TryParse(number,
                 System.Globalization.NumberStyles.Any,
                 System.Globalization.NumberFormatInfo.InvariantInfo,
                 out numberParsed);
 
             return isNumber;
         }
+        #endregion
+
     }
 }
