74dad24f299f74fe70008f7c6d43679601f96548
Gabriel Bur <gabriel.bur@outlook.com>
2019-08-21 21:54:48 -0300

    creating calculator project

---
+++ AspNetUdemy-01-Calculator/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Calculator")]
+    public class CalculatorController : Controller
+    {
+        // Get api/Calculator/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + 
+                    ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+
+
+
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal result;
+
+            if (decimal.TryParse(number,out result))
+            {
+                return result;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            double numberParsed;
+
+            bool isNumber = double
+                .TryParse(number, 
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out numberParsed);
+
+            return isNumber;
+        }
+    }
+}
