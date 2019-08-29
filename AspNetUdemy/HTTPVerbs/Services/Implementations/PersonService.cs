5d6f6f83c69ac3db0ae157fddf76cd339bc77b0b
Gabriel Bur <gabriel.bur@outlook.com>
2019-08-28 22:51:22 -0300

    changes in model and service

--- AspNetUdemy/HTTPVerbs/Services/Implementations/PersonService.cs
+++ AspNetUdemy/HTTPVerbs/Services/Implementations/PersonService.cs
@@ -1,33 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using HTTPVerbs.Context;
 using HTTPVerbs.Model;
 
 namespace HTTPVerbs.Services.Implementations
 {
     public class PersonService : IPersonService
     {
+        private readonly MySqlContext _context;
+        public PersonService(MySqlContext context)
+        {
+            _context = context;
+        }
+
+
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
             return person;
         }
 
         public void Delete(long id)
         {
-            //
+            var selectedPerson = _context.Persons
+               .SingleOrDefault(x => x.Id.Equals(id));
+
+            if (selectedPerson != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(selectedPerson);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
         }
 
         public Person FindById(long id)
         {
-            return new Person();
+            return _context.Persons.SingleOrDefault(x => x.Id.Equals(id));
         }
 
         public List<Person> FingAll()
         {
-            return new List<Person>();
+            try
+            {
+                return _context.Persons.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public Person Update(Person person)
         {
+            var selectedPerson =_context.Persons
+                .SingleOrDefault(x => x.Id.Equals(person.Id));
+
+            if(selectedPerson != null)
+            {
+                try
+                {
+                    _context.Entry(selectedPerson).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                return new Person();
+            }
+
             return person;
         }
     }
